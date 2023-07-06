0a529d8c2bda4f12fe5d8d09c412d2cc590b1365
unknown <marwan.nasser565@gamil.com>
2023-07-06 11:01:35 +0300

    last update

--- resources/views/livewire/doctor-interface.blade.php
+++ resources/views/livewire/doctor-interface.blade.php
@@ -3,6 +3,10 @@
     <div class="row">
         <div class="col-lg-3 ps-0" wire:poll.30000ms>
             <p class="mb-2">{{ __('patients') }} :</p>
+            <div class="box-plan">
+                <h5 class="name">خطة رقم</h5>
+                <p class="num">1</p>
+            </div>
             <ul class="list-unstyled main-nav-tap mb-3">
                 <li class="nav-item" wire:click='$set("patients_screen","transfers")'>
                     <a href="#" class="nav-link active cursor-auto">
@@ -51,42 +55,72 @@
             </div>
         </div>
         <div class="col-lg-9 mt-3 mt-lg-0">
-            <div class="d-flex mb-1 align-items-center">
-                <p class="mb-0">
-                    {{ __('Patient name') }} :
-                    {{ $patient->name ?? null }}
-                </p>
+            <div class="d-flex mb-1 align-items-center justify-content-between">
+                <div class="d-flex mb-1 align-items-center gap-1">
+                    <p class="mb-0">
+                        {{ __('Patient name') }} :
+                        {{ $patient->name ?? null }}
+                    </p>
 
-                @if ($patient)
-                <div class="mb-0">
-                    <button type="submit" wire:click="endSession" class="btn btn-sm btn-info me-2">
-                        {{ __('End Session') }}
-                    </button>
-                </div>
-                @endif
-
-                @if ($patient->sugar ?? null or $patient->pressure ?? null or $patient->is_pregnant ?? null)
-
-                <div class="alert alert-danger d-flex align-items-center mb-0 py-2 ps-3 pe-2 me-2" role="alert">
-                    <svg class="bi flex-shrink-0 ms-2 resize-svg-Bo" role="img" aria-label="Warning:">
-                        <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-                    </svg>
-                    <div>
-                        {{ __('The patient suffers from') }} :
-                        @if ($patient->sugar)
-                        <span>{{ __('diabetes') }},</span>
-                        @endif
-                        @if ($patient->pressure)
-                        <span>{{ __('high blood pressure') }}, </span>
-                        @endif
-                        @if ($patient->is_pregnant)
-                        <span>{{ __('pregnant') }}</span>
-                        @endif
-
+                    @if ($patient)
+                    <div class="mb-0">
+                        <button type="submit" wire:click="endSession" class="btn btn-sm btn-info me-2">
+                            {{ __('End Session') }}
+                        </button>
                     </div>
-                </div>
-                @endif
+                    @endif
 
+                    @if ($patient->sugar ?? null or $patient->pressure ?? null or $patient->is_pregnant ?? null)
+                    <div class="alert alert-danger d-flex align-items-center mb-0 py-2 ps-3 pe-2 me-2" role="alert">
+                        <svg class="bi flex-shrink-0 ms-2 resize-svg-Bo" role="img" aria-label="Warning:">
+                            <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                        </svg>
+                        <div>
+                            {{ __('The patient suffers from') }} :
+                            @if ($patient->sugar)
+                            <span>{{ __('diabetes') }},</span>
+                            @endif
+                            @if ($patient->pressure)
+                            <span>{{ __('high blood pressure') }}, </span>
+                            @endif
+                            @if ($patient->is_pregnant)
+                            <span>{{ __('pregnant') }}</span>
+                            @endif
+                        </div>
+                    </div>
+                    @endif
+                </div>
+                <div class="btn-holder">
+                    <button class="btn-main-sm blue-color">كشف حاله</button>
+                </div>
+
+            </div>
+            <div class="taps-holder mb-3">
+                <ul class="nav nav-pills main-nav-tap mb-3 flex-wrap gap-4">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-center">
+                            جلسه 1
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-center">
+                            جلسه 2
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-center">
+                            جلسه 3
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-center">
+                            جلسه 4
+                        </a>
+                    </li>
+                </ul>
+                <div class=" main-tab-content">
+                    المحتوي قريبا...
+                </div>
             </div>
             <div>
                 {{-- alert success --}}
