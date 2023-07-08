f7e8d810b450368fe4c514f1925d74adfd962f53
amrelnamory <amro.elnamory94@gmail.com>
2023-07-08 14:53:10 +0300

    Update doctor-interface.blade.php

--- resources/views/livewire/doctor-interface.blade.php
+++ resources/views/livewire/doctor-interface.blade.php
@@ -4,12 +4,14 @@
         <div class="col-lg-3 col-xl-2 ps-0" wire:poll.30000ms>
             <p class="mb-2">{{ __('patients') }} :</p>
             @if ($patient && $patient->packages->count() > 0)
-            @foreach ($patient->packages as $item)
-            <div class="box-plan">
-                <button wire:click="selectPackage({{ $item->id }})" class="name btn">{{ $item->package->title
-                    }}</button>
-            </div>
-            @endforeach
+                @foreach ($patient->packages as $item)
+                    @if ($item->diagnoses->count() < $item->dayes_period)
+                        <div class="box-plan">
+                            <button wire:click="selectPackage({{ $item->id }})"
+                                class="name btn">{{ $item->package->title }}</button>
+                        </div>
+                    @endif
+                @endforeach
 
             @endif
             <ul class="list-unstyled main-nav-tap mb-3">
@@ -23,14 +25,14 @@
             <div class=" main-tab-content">
                 <ul class=" d-flex flex-wrap gap-2">
                     @forelse(doctor()->appointments()->Transferred()->doesntHave('diagnos')->get() as $appointment)
-                    <li class="right-b color-gr">
-                        <a href="#" wire:click="selectPatient({{ $appointment->id }})">
-                            {{ $appointment->patient->name }} <br> {{ $appointment->appointment_date }} |
-                            {{ date('g:iA', strtotime($appointment->appointment_time)) }}</a>
-                    </li>
-                    <hr>
+                        <li class="right-b color-gr">
+                            <a href="#" wire:click="selectPatient({{ $appointment->id }})">
+                                {{ $appointment->patient->name }} <br> {{ $appointment->appointment_date }} |
+                                {{ date('g:iA', strtotime($appointment->appointment_time)) }}</a>
+                        </li>
+                        <hr>
                     @empty
-                    <li class="color-gr">{{ __('There is no') }}</li>
+                        <li class="color-gr">{{ __('There is no') }}</li>
                     @endforelse
                 </ul>
             </div>
@@ -45,37 +47,37 @@
             <div class=" main-tab-content">
                 <ul class="{{ $patients_screen == 'today' ? '' : '' }}">
                     @forelse($today_appointments as $appointment)
-                    <li class="right-b alt-text-color"><a href="#" class="alt-text-color"
-                            wire:click="selectPatient({{ $appointment->id }})">
-                            {{ $appointment->patient->name }} <br> {{ $appointment->appointment_date }} |
-                            {{ date('g:iA', strtotime($appointment->appointment_time)) }}
-                            <br>
-                            {{ __('Attended at') }} {{ $appointment->attended_at }}
-                        </a>
+                        <li class="right-b alt-text-color"><a href="#" class="alt-text-color"
+                                wire:click="selectPatient({{ $appointment->id }})">
+                                {{ $appointment->patient->name }} <br> {{ $appointment->appointment_date }} |
+                                {{ date('g:iA', strtotime($appointment->appointment_time)) }}
+                                <br>
+                                {{ __('Attended at') }} {{ $appointment->attended_at }}
+                            </a>
 
-                    </li>
+                        </li>
                     @empty
-                    <li class="alt-text-color">{{ __('There is no') }}</li>
+                        <li class="alt-text-color">{{ __('There is no') }}</li>
                     @endforelse
                 </ul>
             </div>
         </div>
         <div class="col-lg-9 col-xl-10 mt-3 mt-lg-0">
             <div class="d-flex mb-3 align-items-center justify-content-between">
-                    <p class="mb-0">
-                        <b class="text-main-color">{{ __('Patient name') }} : </b>
-                        {{ $patient->name ?? null }}
-                        @if ($patient_package)
-                            -
+                <p class="mb-0">
+                    <b class="text-main-color">{{ __('Patient name') }} : </b>
+                    {{ $patient->name ?? null }}
+                    @if ($patient_package)
+                        -
                         <b class="text-main-color">اسم الخطة : </b> {{ $patient_package->package->title }}
-                        @endif
-                        @if ($session_no)
+                    @endif
+                    @if ($session_no)
                         جلسة {{ $session_no }}
-                        @endif
-                    </p>
+                    @endif
+                </p>
 
 
-                    @if ($patient->sugar ?? null or $patient->pressure ?? null or $patient->is_pregnant ?? null)
+                @if ($patient->sugar ?? null or $patient->pressure ?? null or $patient->is_pregnant ?? null)
                     <div class="alert alert-danger d-flex align-items-center mb-0 py-2 ps-3 pe-2 me-2" role="alert">
                         <svg class="bi flex-shrink-0 ms-2 resize-svg-Bo" role="img" aria-label="Warning:">
                             <path
@@ -84,27 +86,28 @@
                         <div>
                             {{ __('The patient suffers from') }} :
                             @if ($patient->sugar)
-                            <span>{{ __('diabetes') }},</span>
+                                <span>{{ __('diabetes') }},</span>
                             @endif
                             @if ($patient->pressure)
-                            <span>{{ __('high blood pressure') }}, </span>
+                                <span>{{ __('high blood pressure') }}, </span>
                             @endif
                             @if ($patient->is_pregnant)
-                            <span>{{ __('pregnant') }}</span>
+                                <span>{{ __('pregnant') }}</span>
                             @endif
                         </div>
                     </div>
-                    @endif
-                    <div class="d-flex align-items-center gap-1 justify-content-between">
+                @endif
+                <div class="d-flex align-items-center gap-1 justify-content-between">
                     @if ($patient)
                         <button type="submit" wire:click="endSession" class="btn btn-sm btn-info me-2">
                             {{ __('End Session') }}
                         </button>
                     @endif
-                        @if ($patient)
-                            <a target="_blank" class="btn btn-sm btn-info" href="{{ route('doctor.invoices.index',['patient'=>$patient->id]) }}">فواتير المريض</a>
-                        @endif
-                    </div>
+                    @if ($patient)
+                        <a target="_blank" class="btn btn-sm btn-info"
+                            href="{{ route('doctor.invoices.index', ['patient' => $patient->id]) }}">فواتير المريض</a>
+                    @endif
+                </div>
                 {{-- @if ($patient)
                 <div class="btn-holder">
                     <button class="btn-main-sm blue-color" wire:click="examine_patient">كشف حاله</button>
@@ -113,88 +116,88 @@
 
             </div>
             @if ($patient && $patient->packages->count() > 0 && $patient_package)
-            <div class="taps-holder mb-3">
-                <ul class="nav nav-pills mb-3 flex-wrap gap-2">
-                    @for ($i = 1; $i <= $patient_package->dayes_period; $i++)
-                        <li class="nav-item">
-                            @php
-                            $diagnose = \App\Models\Diagnose::where('patient_id', $this->patient->id)
-                            ->where('patient_package_id', $patient_package->id)
-                            ->where('session_no', $i)
-                            ->first();
-
-                            @endphp
-                            <button wire:click="selectSession({{ $patient_package->id }}, {{ $i }})"
-                                class="nav-link text-center {{ $diagnose ? 'bg-gray' : 'bg-green' }}">
-                                جلسة {{ $i }}
-                            </button>
-                        </li>
+                <div class="taps-holder mb-3">
+                    <ul class="nav nav-pills mb-3 flex-wrap gap-2">
+                        @for ($i = 1; $i <= $patient_package->dayes_period; $i++)
+                            <li class="nav-item">
+                                @php
+                                    $diagnose = \App\Models\Diagnose::where('patient_id', $this->patient->id)
+                                        ->where('patient_package_id', $patient_package->id)
+                                        ->where('session_no', $i)
+                                        ->first();
+                                    
+                                @endphp
+                                <button wire:click="selectSession({{ $patient_package->id }}, {{ $i }})"
+                                    class="nav-link text-center {{ $diagnose ? 'bg-gray' : 'bg-green' }}">
+                                    جلسة {{ $i }}
+                                </button>
+                            </li>
                         @endfor
-                </ul>
-            </div>
+                    </ul>
+                </div>
             @endif
             <div>
                 {{-- alert success --}}
                 @if (session()->has('success'))
-                <div class="alert alert-success">
-                    {{ session('success') }}
-                </div>
+                    <div class="alert alert-success">
+                        {{ session('success') }}
+                    </div>
                 @endif
             </div>
             @if ($patient || $examine_session == true)
 
 
-            @if ($patient_package && !$session_no)
-            <div class="alert alert-danger">يجب تحديد الجلسة أولاً</div>
-            @else
-            <ul class="nav nav-pills main-nav-tap mb-3" style="flex-wrap: wrap !important;">
-                <li class="nav-item" wire:click="$set('screen','current')">
-                    <a href="#" class="nav-link {{ $screen == 'current' ? 'active' : '' }}">
-                        {{ __('current diagnosis') }}
-                    </a>
-                </li>
-                <li class="nav-item" wire:click="$set('screen','invoice')">
-                    <a href="#" class="nav-link {{ $screen == 'invoice' ? 'active' : '' }}">
-                        {{ __('Issuance of invoice') }}
-                    </a>
-                </li>
-                <li class="nav-item" wire:click="$set('screen','data')">
-                    <a href="#" class="nav-link {{ $screen == 'data' ? 'active' : '' }}">
-                        {{ __('Patient data') }}
-                    </a>
-                </li>
-                <li class="nav-item" wire:click="$set('screen','prev')">
-                    <a href="#" class="nav-link  {{ $screen == 'prev' ? 'active' : '' }}">
-                        {{ __('previous diagnoses') }}
-                    </a>
-                </li>
-                <li class="nav-item" wire:click="$set('screen','trans')">
-                    <a href="#" class="nav-link {{ $screen == 'trans' ? 'active' : '' }}  ">
-                        {{ __('Transfer of the patient') }}
-                    </a>
-                </li>
+                @if ($patient_package && !$session_no)
+                    <div class="alert alert-danger">يجب تحديد الجلسة أولاً</div>
+                @else
+                    <ul class="nav nav-pills main-nav-tap mb-3" style="flex-wrap: wrap !important;">
+                        <li class="nav-item" wire:click="$set('screen','current')">
+                            <a href="#" class="nav-link {{ $screen == 'current' ? 'active' : '' }}">
+                                {{ __('current diagnosis') }}
+                            </a>
+                        </li>
+                        <li class="nav-item" wire:click="$set('screen','invoice')">
+                            <a href="#" class="nav-link {{ $screen == 'invoice' ? 'active' : '' }}">
+                                {{ __('Issuance of invoice') }}
+                            </a>
+                        </li>
+                        <li class="nav-item" wire:click="$set('screen','data')">
+                            <a href="#" class="nav-link {{ $screen == 'data' ? 'active' : '' }}">
+                                {{ __('Patient data') }}
+                            </a>
+                        </li>
+                        <li class="nav-item" wire:click="$set('screen','prev')">
+                            <a href="#" class="nav-link  {{ $screen == 'prev' ? 'active' : '' }}">
+                                {{ __('previous diagnoses') }}
+                            </a>
+                        </li>
+                        <li class="nav-item" wire:click="$set('screen','trans')">
+                            <a href="#" class="nav-link {{ $screen == 'trans' ? 'active' : '' }}  ">
+                                {{ __('Transfer of the patient') }}
+                            </a>
+                        </li>
 
-                @if (env('PHARMACY_ENABLED', false))
-                <li class="nav-item" wire:click="$set('screen','pharmacy')">
-                    <a href="#" class="nav-link {{ $screen == 'pharmacy' ? 'active' : '' }}  ">
-                        {{ __('dispensing medicines') }}
-                    </a>
-                </li>
+                        @if (env('PHARMACY_ENABLED', false))
+                            <li class="nav-item" wire:click="$set('screen','pharmacy')">
+                                <a href="#" class="nav-link {{ $screen == 'pharmacy' ? 'active' : '' }}  ">
+                                    {{ __('dispensing medicines') }}
+                                </a>
+                            </li>
+                        @endif
+                        <li class="nav-item" wire:click="$set('screen','review')">
+                            <a href="#" class="nav-link {{ $screen == 'review' ? 'active' : '' }}">
+                                مراجعة
+                            </a>
+                        </li>
+
+                    </ul>
+
+                    <div class=" main-tab-content">
+                        @include('doctor.interfaces.' . $screen)
+                    </div>
                 @endif
-                <li class="nav-item" wire:click="$set('screen','review')">
-                    <a href="#" class="nav-link {{ $screen == 'review' ? 'active' : '' }}">
-                        مراجعة
-                    </a>
-                </li>
-
-            </ul>
-
-            <div class=" main-tab-content">
-                @include('doctor.interfaces.' . $screen)
-            </div>
-            @endif
             @else
-            <div class="alert alert-danger">{{ __('Please click on the patients name') }}</div>
+                <div class="alert alert-danger">{{ __('Please click on the patients name') }}</div>
             @endif
 
         </div>
